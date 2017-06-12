bcf67bf4a0488763bf9478dbccf5b8503ce1d436
Fredrik Stolpe <fredrik.stolpe@gmail.com>
2017-06-12 21:16:24 +0200

    Added rpi-gpio lib and poc code

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 var validate = require('express-validation');
 var validation = require('../validation/');
+var Pin = require('../modules/pin.js');
+
+var neckPin = new Pin(19);
+neckPin.setupOutput();
+var pin2 = new Pin(11);
+var pin3 = new Pin(21);
 
 router.get('/neck/:action', validate(validation.api.neck.get), function(req, res, next) {
+  neckPin.write(req.params.action == 'up');
   res.json({
     success: true,
     result: true
