fada992dd2fbafe96c7ceeb353ae18e25d1003fa
Fredrik Stolpe <fredrik.stolpe@gmail.com>
2017-06-13 08:59:43 +0200

    Configured pins

--- routes/api.js
+++ routes/api.js
@@ -6,29 +6,35 @@ var Pin = require('../modules/pin.js');
 
 var neckPin = new Pin(19);
 neckPin.setupOutput();
-var pin2 = new Pin(11);
-var pin3 = new Pin(21);
+
+var mouthPin = new Pin(21);
+mouthPin.setupOutput();
+
+var tailPin = new Pin(23);
+tailPin.setupOutput();
 
 router.get('/neck/:action', validate(validation.api.neck.get), function(req, res, next) {
   neckPin.write(req.params.action == 'up');
   res.json({
     success: true,
-    result: true
-  });  
+    result: req.params.action
+  });
 });
 
 router.get('/mouth/:action', validate(validation.api.mouth.get), function(req, res, next) {
+  mouthPin.write(req.params.action == 'open');
   res.json({
     success: true,
-    result: true
-  }); 
+    result: req.params.action
+  });
 });
 
 router.get('/tail/:action', validate(validation.api.tail.get), function(req, res, next) {
+  tailPin.write(req.params.action == 'up');
   res.json({
     success: true,
-    result: true
-  }); 
+    result: req.params.action
+  });
 });
 
 module.exports = router;
